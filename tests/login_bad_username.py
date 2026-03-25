9317f4ca3be0bf60ec0476dac7868a250382256e
Andy <speedy.stowaway@proton.me>
2026-03-24 22:43:38 -0500

    Add tests for login errors: bad password and bad username.

---
+++ tests/login_bad_username.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import title_is
+
+class UserInfo:
+    username = "BADUSERNAME"
+    password = "SuperSecretPassword!"
+
+# Open Web Session on Firefox
+driver = (webdriver.Firefox())
+driver.get('https://the-internet.herokuapp.com/')
+
+# Print Website Title
+title = driver.title
+print(title)
+
+# Navigate to /login
+formLink = driver.find_element(By.LINK_TEXT, 'Form Authentication')
+formLink.click()
+
+# Finds username and password names and fill them
+username = driver.find_element(By.ID, 'username')
+password = driver.find_element(By.NAME, 'password')
+username.send_keys(UserInfo.username)
+password.send_keys(UserInfo.password)
+
+# Click Login
+login = driver.find_element(By.CSS_SELECTOR, 'i.fa-sign-in')
+login.click()
+# session.quit()
