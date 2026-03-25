fe82af1c67c096f13b29fb833b6e1081b4c44cef
Andy <speedy.stowaway@proton.me>
2026-03-24 22:34:31 -0500

    changed to successful login, swapped session to driver, specified Login selector more, created UserInfo class for modularity in the future

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,25 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.expected_conditions import title_is
 
+class UserInfo:
+    username = "tomsmith"
+    password = "SuperSecretPassword!"
+
 # Open Web Session on Firefox
-session = (webdriver.Firefox())
-session.get('https://the-internet.herokuapp.com/')
+driver = (webdriver.Firefox())
+driver.get('https://the-internet.herokuapp.com/')
 
 # Print Website Title
-title = session.title
+title = driver.title
 print(title)
 
 # Navigate to /login
-formLink = session.find_element(By.LINK_TEXT, 'Form Authentication')
+formLink = driver.find_element(By.LINK_TEXT, 'Form Authentication')
 formLink.click()
 
 # Finds username and password names and fill them
-username = session.find_element(By.ID, 'username')
-password = session.find_element(By.NAME, 'password')
-username.send_keys('tomsmith')
-password.send_keys('SuperSecretPassword')
+username = driver.find_element(By.ID, 'username')
+password = driver.find_element(By.NAME, 'password')
+username.send_keys(UserInfo.username)
+password.send_keys(UserInfo.password)
 
 # Click Login
-login = session.find_element(By.CSS_SELECTOR, 'i')
+login = driver.find_element(By.CSS_SELECTOR, 'i.fa-sign-in')
 login.click()
 # session.quit()
