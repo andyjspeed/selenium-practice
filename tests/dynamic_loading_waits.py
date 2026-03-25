5e4bb6473f15375c28cf24439cc99f53970a8e31
Andy <speedy.stowaway@proton.me>
2026-03-25 17:48:00 -0500

    refactor dynamic loading sript to go through both examples

--- tests/dynamic_loading_waits.py
+++ tests/dynamic_loading_waits.py
@@ -7,28 +7,29 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = (webdriver.Firefox())
 driver.get('https://the-internet.herokuapp.com/')
 
-# Navigate to /login
+# Navigate to the Dynamic Loading Page using links.
 formLink = driver.find_element(By.LINK_TEXT, 'Dynamic Loading')
 formLink.click()
-"""""
-# Finds the link for
-example2 = driver.find_element(By.PARTIAL_LINK_TEXT, 'Example 2')
-example2.click()
 
-startbutton = driver.find_element(By.CSS_SELECTOR, 'button')
-startbutton.click()
-wait = WebDriverWait(driver, 10)
-element = wait.until(EC.element_to_be_clickable((By.ID, 'finish')))
-print(element.text)
-
-driver.back()
-"""
-
+# Starting Example 1 Test
 example1 = driver.find_element(By.PARTIAL_LINK_TEXT, 'Example 1')
 example1.click()
 startbutton = driver.find_element(By.CSS_SELECTOR, 'button')
 startbutton.click()
+# Wait for Example 1 Test
 wait = WebDriverWait(driver, 10)
 element = wait.until(EC.visibility_of_element_located((By.ID, 'finish')))
+print(element.text)
+driver.back()
+
+
+# Starting Example 2 Test
+example2 = driver.find_element(By.PARTIAL_LINK_TEXT, 'Example 2')
+example2.click()
+startbutton = driver.find_element(By.CSS_SELECTOR, 'button')
+startbutton.click()
+# Wait for Example 2 Test
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.element_to_be_clickable((By.ID, 'finish')))
 print(element.text)
 # driver.quit()
